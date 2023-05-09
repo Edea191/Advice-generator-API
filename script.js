520e93273a607acd465a241218da44bc73c51952
Dawid <deswid@interia.pl>
2023-05-09 23:16:07 +0200

    Create API

---
+++ script.js
@@ -0,0 +1,22 @@
+
+update();
+function update(){
+
+
+fetch('https://api.adviceslip.com/advice').
+then(res =>{
+    if(res.ok){
+        return res.json();  
+    }
+    else{
+        return Promise.reject(`Http error: ${res.status}`);
+    }
+}).
+then(res =>
+     {
+        document.getElementById('AdviceID').value="Advice #"+res.slip.id;
+        document.getElementById('AdviceText').value=res.slip.advice;
+    })
+.catch(error => {console.error(error)});
+
+}
